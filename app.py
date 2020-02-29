211d98fcfeda315cc6849f4e918eca6bd761d698
AlenaNik <alena.nik.digital@gmail.com>
2020-02-29 20:12:23 +0100

    elsif

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-first = 'John'
-last = 'Smith'
-print(f'{first}')
+weight = input('Enter your weight: ')
+measure = input("The measure you've put here is (L)bs or (K)kg?  ")
+
+if measure == 'L' or 'l':
+    print(f'{int(weight) * 2.205} you weight in kg')
+elif measure == 'K' or 'k':
+    print(f'{int(weight) / 2.205} you weight in lbs')
+else:
+    print('To bien')
