c9b4d48c5e3e4a971e3697911a52e60e6ab12f73
AlenaNik <alena.nik.digital@gmail.com>
2020-03-01 11:25:51 +0100

    nested loop

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
-command = ""
-started = False
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        if started:
-            print('Car is already started')
-        else:
-            started = True
-            print("Ready to go")
-    elif command == "stop":
-        if not started:
-            print('Car is already stopped')
-        else:
-            started = False
-            print("Stopping the car")
-    elif command == "help":
-        print("""
-        start - to start,
-        stop - top stop,
-        quit - to quit
-        """)
-    elif command == "quit":
-        exit()
-    else:
-        print("Sorry no command recognized")
-
-
+numbers = [5, 2, 5, 2, 2]
+for num in numbers:
+    output = ''
+    for x in range(num):
+        output += 'x'
+    print(output)
