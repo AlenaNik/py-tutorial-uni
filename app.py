ac0d46622995484be0ff995f71758d2840f52b71
AlenaNik <alena.nik.digital@gmail.com>
2020-03-01 21:24:16 +0100

    creating a package

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-try:
-    age = int(input('Age: '))
-    income = int(input('Income: '))
-    print(age / income)
-except ValueError:
-    print('Enter a number')
-except ZeroDivisionError:
-    print('Cant be zero')
+from ecommerce.shipping import calculate_shipping
