0cd74bc2437a036ce73f09e98d9ce4ccf8d38aef
AlenaNik <alena.nik.digital@gmail.com>
2020-03-01 14:43:06 +0100

    largest num in arr

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-numbers = [5, 2, 5, 2, 2]
+numbers = [67, 2, 3, 4, 7, 8, 1, 2, 3, 4]
+largest = numbers[0]
 for num in numbers:
-    output = ''
-    for x in range(num):
-        output += 'x'
-    print(output)
+    if num > largest:
+        largest = num
+print(largest)
