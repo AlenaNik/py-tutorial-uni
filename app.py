bee5ebf79ec18819cb27b29a4761a49bb5630631
AlenaNik <alena.nik.digital@gmail.com>
2020-03-01 18:36:20 +0100

    handling errors

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-message = input("> ")
-words = message.split(' ')
-smiles = {
-    ":)": "🙂",
-    ":(": "😟",
-    "house": "🏡"
-}
-output = ""
-for word in words:
-   output += smiles.get(word, word) + " "
-print(output)
+try:
+    age = int(input('Age: '))
+    income = int(input('Income: '))
+    print(age / income)
+except ValueError:
+    print('Enter a number')
+except ZeroDivisionError:
+    print('Cant be zero')
