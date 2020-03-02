e5d5a1ce82ffc6734a81930ca321216352a4ec61
AlenaNik <alena.nik.digital@gmail.com>
2020-03-02 08:34:46 +0100

    xlsx update

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from pathlib import Path
+import openpyxl as xl
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1, 1)
 
-path = Path()
-for file in path.glob('*.py'):
-    print(file)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+wb.save('transactions2.xlsx')
